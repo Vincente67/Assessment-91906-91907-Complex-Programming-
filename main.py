ad4e1203da757094129701694facf39015b58025
Vincente67 <vincente_allan@paraparaumucollege.school.nz>
2026-08-18 13:00:42 +1200

    Add header comments to main.py
    Added program header comments for Turtle Shape Designer.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# -------------------------------------------------------------------
+# Turtle Shape Designer 
+# AS91906 / AS91907
+# 
+# This program allows Year 10 students to draw their own 
+# Turtle shapes using mouse clicks.
+#
+# Complex techniques used:
+# 1. Graphical User Interface (GUI)
+# 2. Writing data to files 
+#
+# Additional techniques 
+# - Turtle non-core library` 
+# - List of coordinate tuples 
+#------------------------------------------------------------------
 
